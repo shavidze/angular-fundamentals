c450f970773f3ce1d1f593ff7ee598b0269b7901
saba <sabashavidze@gmail.com>
2019-06-17 12:44:01 +0400

    add

---
+++ src/app/events/event-list.component.ts
@@ -0,0 +1,14 @@
+
+import {Component} from '@angular/core'
+
+@Component({
+    selector:"event-list",
+    template:`
+    <div>
+        <h1>Upcomming Angular Events</h1>
+    </div>
+    `
+})
+export class EventsListComponent{
+
+}
