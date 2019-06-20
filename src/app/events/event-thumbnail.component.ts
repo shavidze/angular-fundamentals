5e7b4480a32401120fc814980275ff735ceaee8e
Saba Shavidze <shavidze>
2019-06-20 14:38:04 +0400

    play with services

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,21 +4,32 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
   selector: "event-thumbnail",
   template: `
     <div class="well hoverwell thumbnail">
-      <h2>{{ event.name }}</h2>
-      <div>Date: {{ event.date }}</div>
-      <div>Time: {{ event.time }}</div>
+      <h2>{{ event?.name }}</h2>
+      <div>Date: {{ event?.date }}</div>
+      <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+        Time: {{ event?.time }}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
       <div>Price: \${{ event.price }}</div>
-      <div>
-        <span>Location: </span>
+      <div *ngIf="event?.location">
+        <span>Location: {{ event?.location?.address }} </span>
         <span>&nbsp;</span>
         <span class="pad-left"
-          >{{ event.location.city }}, {{ event.location.country }}</span
+          >{{ event?.location.city }}, {{ event?.location.country }}</span
         >
       </div>
     </div>
   `,
   styles: [
     `
+      .green {
+        color: #003300 !important;
+      }
+      .bold {
+        font-weight: bold;
+      }
       .pad-left {
         margin-left: 15px;
       }
@@ -30,4 +41,10 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+  getStartTimeClass() {
+    if (this.event && this.event.time === "8:00 am") {
+      return ["green bold"];
+    }
+    return [];
+  }
 }
